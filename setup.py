f5d824aab20ae700ba75b254197f3a548076fa51
khorevkp <khorevkp@gmail.com>
2022-03-12 22:05:19 +0100

    1st commit

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='kktools',
-    version='1.0',
+    version='1.1',
     author='Konstantin Khorev',
     author_email='khorevkp@gmail.com',
     description='Tools for finance and treasury specialists',
