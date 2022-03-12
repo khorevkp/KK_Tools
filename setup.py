64008e2db1948110d2c1f5f1f90a6f58694d383c
khorevkp <khorevkp@gmail.com>
2022-03-12 12:17:57 +0100

    1st commit

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='kktools',  # package name
-    version='1.0',  # version
+    name='kktools',
+    version='1.0',
     author='Konstantin Khorev',
     author_email='khorevkp@gmail.com',
-    description='Tools for finance and treasury specialists',  # short description
-    url='https://github.com/khorevkp/KK_Tools',  # package URL
-    install_requires=['pandas'],  # list of packages this package depends on
-    packages=setuptools.find_packages(exclude=['tests*']),
+    description='Tools for finance and treasury specialists',
+    url='https://github.com/khorevkp/KK_Tools',
+    install_requires=['pandas']
 )
