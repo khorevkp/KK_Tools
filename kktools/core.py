43fd2f86eb12ce3d6eea47ae5ab8aee647a45943
khorevkp <khorevkp@gmail.com>
2022-07-09 09:22:41 +0200

    1st commit

--- kktools/core.py
+++ kktools/core.py
@@ -2,29 +2,6 @@ import pandas as pd
 import requests
 import xml.etree.ElementTree as ET
 
-def df_to_excel(file_name, df, worksheet_name="Sheet1", max_length=25):
-    with pd.ExcelWriter(file_name, engine='xlsxwriter', datetime_format='dd/mm/yyyy') as writer:
-
-        df.to_excel(writer, sheet_name=worksheet_name, index=False)
-        workbook = writer.book
-        worksheet = writer.sheets[worksheet_name]
-        worksheet.autofilter(0, 0, df.shape[0], df.shape[1] - 1)
-        format_num = workbook.add_format({'num_format': '#,##0.00'})
-        format_date = workbook.add_format({'num_format': 'mm/dd/yyyy'})
-        k = 0
-        for column in list(df.columns):
-            series = df[column]
-            max_len = max((
-                series.astype(str).map(len).max(),  # len of largest item
-                len(str(series.name))  # len of column name/header
-                )) + 1  # adding a little extra space
-            max_len = min(max_len, max_length)  # we want to limit still the length of the column with max_length
-            if df.dtypes[column] == 'float64':
-                worksheet.set_column(k, k, max_len, format_num)
-            else:
-                worksheet.set_column(k, k, max_len)
-            k = k + 1
-
 def get_ecb_rates(currency, start_period):
     # please see description of request structure here: https://sdw-wsrest.ecb.europa.eu/help/
 
@@ -92,7 +69,31 @@ def get_last_ecb_rates():
 
     return df
 
-def dfs_to_excel(file_name, df_list, max_length=25):
+def df_to_excel(file_name, df, worksheet_name="Sheet1", max_length=25):
+    with pd.ExcelWriter(file_name, engine='xlsxwriter', datetime_format='dd/mm/yyyy') as writer:
+
+        df.to_excel(writer, sheet_name=worksheet_name, index=False)
+        workbook = writer.book
+        worksheet = writer.sheets[worksheet_name]
+        worksheet.autofilter(0, 0, df.shape[0], df.shape[1] - 1)
+        format_num = workbook.add_format({'num_format': '#,##0.00'})
+        format_date = workbook.add_format({'num_format': 'mm/dd/yyyy'})
+        k = 0
+        for column in list(df.columns):
+            series = df[column]
+            max_len = max((
+                series.astype(str).map(len).max(),  # len of largest item
+                len(str(series.name))  # len of column name/header
+                )) + 1  # adding a little extra space
+            max_len = min(max_len, max_length)  # we want to limit still the length of the column with max_length
+            if df.dtypes[column] == 'float64':
+                worksheet.set_column(k, k, max_len, format_num)
+            else:
+                worksheet.set_column(k, k, max_len)
+            k = k + 1
+
+
+def dfs_to_excel(file_name, df_list, max_length=25, sheet_names = []):
     with pd.ExcelWriter(file_name, engine='xlsxwriter', datetime_format='dd/mm/yyyy') as writer:
 
         s = 1
@@ -101,11 +102,14 @@ def dfs_to_excel(file_name, df_list, max_length=25):
         format_date = workbook.add_format({'num_format': 'mm/dd/yyyy'})
 
         for df in df_list:
-            worksheet_name = "Sheet" + str(s)
+            if sheet_names:
+                worksheet_name = sheet_names[s-1]
+            else:
+                worksheet_name = "Sheet" + str(s)
             s = s + 1
-            df.to_excel(writer, sheet_name=worksheet_name)
+            df.to_excel(writer, sheet_name=worksheet_name, index=False)
             worksheet = writer.sheets[worksheet_name]
-            worksheet.autofilter(0, 0, df.shape[0], df.shape[1])
+            worksheet.autofilter(0, 0, df.shape[0], df.shape[1]-1)
             k = 0
             for column in list(df.columns):
                 series = df[column]
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='kktools',
-    version='2.0',
+    version='2.1',
     author='Konstantin Khorev',
     author_email='khorevkp@gmail.com',
     description='Tools for finance and treasury specialists',
