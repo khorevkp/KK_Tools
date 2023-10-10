4e792ae992d16acd23c025a78ab5be77c60778d5
khorevkp <khorevkp@gmail.com>
2023-10-10 11:27:35 +0200

    1st commit for v.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='kktools',
-    version='2.3',
+    version='2.31',
     author='Konstantin Khorev',
     author_email='khorevkp@gmail.com',
     description='Tools for finance and treasury specialists',
